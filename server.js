e208568674187eecafc229ff17007f62ca10394d
LibbyKnight <lopez.beatriz.s@gmail.com>
2017-04-20 00:21:17 -0400

    added public directory

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ var app = express();
 
 var PORT = 3000;
 
-app.listen(PORT, function() {
-	console.log("Listening on port " + PORT);
-})
-
 
 
 app.get('/', function(req, res) {
 	res.sendfile('/meanTodo/index.html');
 });
+
+app.use(express.static(''))
+
+app.listen(PORT, function() {
+	console.log("Listening on port " + PORT);
+})
+
