5b916f70651a3dda179131060d4c2c49fb13450d
LibbyKnight <lopez.beatriz.s@gmail.com>
2017-04-24 16:03:50 -0400

    fixed list controller file

--- public/states/list/list.controller.js
+++ public/states/list/list.controller.js
@@ -1,7 +1,16 @@
 angular
 	.module('meanTodo.list')
-	.controller('ListCtrl, ListCtrl');
+	.controller('ListCtrl', ListCtrl);
 
 function ListCtrl() {
 	var vm = this;
+		vm.submit = submitFn;
+
+	function submitFn() {
+		console.log(vm);
+
+	}
 }
+
+
+
