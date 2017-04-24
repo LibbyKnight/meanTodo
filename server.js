0b04b58cb68cdffe1b7fae9ad4d59080a08187d2
LibbyKnight <lopez.beatriz.s@gmail.com>
2017-04-24 16:27:45 -0400

    inserted data to db as test

--- server.js
+++ server.js
@@ -6,15 +6,34 @@ var app = express();
 
 var PORT = 3000;
 
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/meanTodos", function(err, db) {
+  if(!err) {
+    console.log("We are connected, y0");
+  }
+
+  	app.use(express.static('public'));
 
 
-// app.get('/', function(req, res) {
-// 	res.sendFile('public/meanTodo/index.html');
-// });r 
+	app.post('/api/todos', function(req, res) {
+		var collection = db.collection("todos");
+		var doc = {task: 'test1', dueDate: 'today', completed: false};
 
-app.use(express.static('public'));
+			/*collection.insert(doc, function(err, ressult){
+				res.send("added, yo");
+			});*/
 
-app.listen(PORT, function() {
-	console.log("Listening on port " + PORT);
-})
+	});
+
+
+	app.listen(PORT, function() {
+		console.log("Listening on port " + PORT);
+	});
+});
+
+
+
 
