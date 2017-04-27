eefc3021c86a91eb2b50ce8a59b8dda9d154217e
LibbyKnight <lopez.beatriz.s@gmail.com>
2017-04-27 14:52:22 -0400

    added fuction to get data

--- public/states/list/list.controller.js
+++ public/states/list/list.controller.js
@@ -2,13 +2,26 @@ angular
 	.module('meanTodo.list')
 	.controller('ListCtrl', ListCtrl);
 
-function ListCtrl() {
+function ListCtrl($http) {
 	var vm = this;
-		vm.submit = submitFn;
+	vm.submit = submitFn;
+	taskList();
+
+	function taskList() {
+		$http({
+			url: "/api/todos",
+			method: "GET",
+		}).then(function(response) {
+			vm.tasks = response.data;
+		}); 
+	}
 
 	function submitFn() {
-		console.log(vm);
-
+		$http({
+			url: "/api/todos",
+			method: "POST",
+			data: {task: vm.task, dueDate: vm.dueDate}
+		});
 	}
 }
 
